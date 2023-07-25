29a858b8a21cf732c56fdd71d32c46340afb697e
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-07-25 20:14:35 +0500

    URL Fix

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'http://api.genossek.mesto.nomoredomains.xyz';
 
 const handleResponseStatus = (response) => {
     if (response.ok) {
