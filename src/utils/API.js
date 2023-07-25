8e3ffed3cde9dd720495c1568c67f6d905cdb074
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-07-25 20:08:02 +0500

    URL Fix

--- src/utils/API.js
+++ src/utils/API.js
@@ -89,10 +89,10 @@ class API {
 }
 
 const api = new API({
-    baseURL: 'http://api.genossek.mesto.nomoredomains.xyz/',
+    baseURL: 'http://api.genossek.mesto.nomoredomains.xyz',
     headers: {
-        authorization: '2bc76956-8c18-424e-a75e-aff99086882b',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
     }
 })
 
